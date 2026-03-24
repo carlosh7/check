22f4effc48122097bf50e01f9994eaf712b836d5
carlosh7 <carlosh7@gmail.com>
2026-03-24 13:21:15 -0500

    Fix: Final V12.6.0 synchronization with cache busting and ID fixes

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ import { API } from './src/frontend/api.js';
 
 /**
  * MASTER SCRIPT
- * Version: V12.5.1
+ * Version: V12.6.0
  * Author: Antigravity
  * 
  * Description: Sistema modular de gestión de asistencia con diseño Chrome Style.
  */
 window.LS = LS;
 window.lazyLoad = lazyLoad;
-console.log('CHECK V12.5.1: Iniciando Sistema Modular...');
+console.log('CHECK V12.6.0: Iniciando Sistema Modular...');
 console.log('[INIT] Script loaded as ESM, LS available');
 
 const App = window.App = {
@@ -21,7 +21,7 @@ const App = window.App = {
         user: null,
         socket: null,
         chart: null,
-        version: '12.5.1',
+        version: '12.6.0',
         groups: [],
         quillEditor: null,
         editingTemplate: null,
