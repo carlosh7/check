0576252ef62a3d55e6b98a5f499c9054e917ab5b
carlosh7 <carlosh7@gmail.com>
2026-03-26 20:40:58 -0500

    bump: version 12.24.1

--- script_v12_16_2.js
+++ script_v12_16_2.js
@@ -15,7 +15,7 @@ import { API } from './src/frontend/api.js';
  */
 window.LS = LS;
 window.lazyLoad = lazyLoad;
-const VERSION = '12.24.0';
+const VERSION = '12.24.1';
 console.log(`CHECK V${VERSION}: Iniciando Sistema Modular...`);
 
 // --- AUTO-UPDATE CACHE V12.16.2 ---
