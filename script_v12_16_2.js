1332a1bb580f8aea2cb848bf041dc00e2202a6d6
carlosh7 <carlosh7@gmail.com>
2026-03-26 21:00:06 -0500

    Fix wheel pointer and firma position in projector layout (V12.24.5)

--- script_v12_16_2.js
+++ script_v12_16_2.js
@@ -15,7 +15,7 @@ import { API } from './src/frontend/api.js';
  */
 window.LS = LS;
 window.lazyLoad = lazyLoad;
-const VERSION = '12.24.4';
+const VERSION = '12.24.5';
 console.log(`CHECK V${VERSION}: Iniciando Sistema Modular...`);
 
 // --- AUTO-UPDATE CACHE V12.16.2 ---
