6b118604dfda0fec268b263f2f4c53698d97f8d8
carlosh7 <carlosh7@gmail.com>
2026-03-27 16:03:24 -0500

    Unificar CSS: Fusionar style.css en modern.css y eliminar conflictos. Resolver FOUC en todo el sitio

--- public/js/app.js
+++ public/js/app.js
@@ -15,7 +15,7 @@ import { API } from './src/frontend/api.js';
  */
 window.LS = LS;
 window.lazyLoad = lazyLoad;
-const VERSION = '12.28.5';
+const VERSION = '12.28.6';
 console.log(`CHECK V${VERSION}: Iniciando Sistema Modular...`);
 
 // --- AUTO-UPDATE CACHE V12.16.2 ---
