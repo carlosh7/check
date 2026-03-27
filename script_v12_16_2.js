201a632508ab1cb42dd3f022f8fbd12c382dc476
carlosh7 <carlosh7@gmail.com>
2026-03-26 21:46:22 -0500

    Reduce wheel container size to fit screen (V12.25.4)

--- script_v12_16_2.js
+++ script_v12_16_2.js
@@ -15,7 +15,7 @@ import { API } from './src/frontend/api.js';
  */
 window.LS = LS;
 window.lazyLoad = lazyLoad;
-const VERSION = '12.25.3';
+const VERSION = '12.25.4';
 console.log(`CHECK V${VERSION}: Iniciando Sistema Modular...`);
 
 // --- AUTO-UPDATE CACHE V12.16.2 ---
