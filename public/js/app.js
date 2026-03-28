bbd05a0ea50ef365b9d3c838cacffd4a2c88569b
carlosh7 <carlosh7@gmail.com>
2026-03-27 22:37:41 -0500

    fix: corregir versión hardcodeada en index.html (12.30.6->12.30.8)

--- public/js/app.js
+++ public/js/app.js
@@ -15,7 +15,7 @@ import { API } from './src/frontend/api.js';
  */
 window.LS = LS;
 window.lazyLoad = lazyLoad;
-const VERSION = '12.30.7';
+const VERSION = '12.30.8';
 console.log(`CHECK V${VERSION}: Iniciando Sistema Modular...`);
 
 // --- VERIFICACIÓN INMEDIATA DE VERSIÓN CARGADA (SIMPLIFICADA) ---
