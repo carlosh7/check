a54a5c1be3622b179f748ddaf2e711a9ad716098
carlosh7 <carlosh7@gmail.com>
2026-03-27 22:10:42 -0500

    feat: npm start ahora instala dependencias automáticamente

--- public/js/app.js
+++ public/js/app.js
@@ -15,7 +15,7 @@ import { API } from './src/frontend/api.js';
  */
 window.LS = LS;
 window.lazyLoad = lazyLoad;
-const VERSION = '12.30.1';
+const VERSION = '12.30.2';
 console.log(`CHECK V${VERSION}: Iniciando Sistema Modular...`);
 
 // --- VERIFICACIÓN INMEDIATA DE VERSIÓN CARGADA (SIMPLIFICADA) ---
