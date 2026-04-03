d96d60555201b232b7ca2abb1d35fd5198263c6b
carlosh7 <carlosh7@gmail.com>
2026-04-03 02:55:47 -0500

    v12.44.32: Sincronizar versiones en todos los archivos

--- public/js/app.js
+++ public/js/app.js
@@ -15,7 +15,7 @@ import { API } from './src/frontend/api.js';
  */
 window.LS = LS;
 window.lazyLoad = lazyLoad;
-const VERSION = '12.44.31';
+const VERSION = '12.44.32';
 console.log(`CHECK V${VERSION}: Iniciando Sistema Modular...`);
 
 // --- VERIFICACIÓN INMEDIATA DE VERSIÓN CARGADA (SIMPLIFICADA) ---
