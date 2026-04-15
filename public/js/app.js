84273d188a01237597fd4abda6dd3e15d0cd5991
Carlos <carlosh7@gmail.com>
2026-04-15 09:31:07 -0500

    v12.44.456 - Sincronizar versiones de imports (12.44.455 -> 12.44.456)

--- public/js/app.js
+++ public/js/app.js
@@ -2,34 +2,34 @@ import { LS, lazyLoad } from './src/frontend/utils.js';
 import { API } from './src/frontend/api.js';
 
 // Imports de nuevos módulos con versión actualizada
-import { Config } from './modules/core/Config.js?v=12.44.452';
-import { AppStateManager } from './modules/core/State.js?v=12.44.452';
-import { Constants } from './modules/utils/Constants.js?v=12.44.452';
-import { RouterManager } from './modules/navigation/Router.js?v=12.44.452';
-import { PersistenceManager } from './modules/navigation/Persistence.js?v=12.44.452';
-import { ToastManager } from './modules/components/Toast.js?v=12.44.452';
-import { ModalManager, hideModal } from './modules/components/Modal.js?v=12.44.452';
-import { TableManager } from './modules/components/Table.js?v=12.44.452';
-import { SidebarManager } from './modules/components/Sidebar.js?v=12.44.452';
-import { FormManager } from './modules/components/Form.js?v=12.44.452';
-import { DropdownManager } from './modules/components/Dropdown.js?v=12.44.452';
-import { ViewManagerInstance } from './modules/views/ViewManager.js?v=12.44.452';
-import { MyEventsViewInstance } from './modules/views/MyEvents.js?v=12.44.452';
-import { AdminViewInstance } from './modules/views/Admin.js?v=12.44.452';
-import { EventConfigViewInstance } from './modules/views/EventConfig.js?v=12.44.452';
-import { SystemViewInstance } from './modules/views/System.js?v=12.44.452';
-import { ApiServiceInstance } from './modules/services/ApiService.js?v=12.44.452';
-import { AuthServiceInstance } from './modules/services/AuthService.js?v=12.44.452';
-import { EventServiceInstance } from './modules/services/EventService.js?v=12.44.452';
-import { GuestServiceInstance } from './modules/services/GuestService.js?v=12.44.452';
+import { Config } from './modules/core/Config.js?v=12.44.456';
+import { AppStateManager } from './modules/core/State.js?v=12.44.456';
+import { Constants } from './modules/utils/Constants.js?v=12.44.456';
+import { RouterManager } from './modules/navigation/Router.js?v=12.44.456';
+import { PersistenceManager } from './modules/navigation/Persistence.js?v=12.44.456';
+import { ToastManager } from './modules/components/Toast.js?v=12.44.456';
+import { ModalManager, hideModal } from './modules/components/Modal.js?v=12.44.456';
+import { TableManager } from './modules/components/Table.js?v=12.44.456';
+import { SidebarManager } from './modules/components/Sidebar.js?v=12.44.456';
+import { FormManager } from './modules/components/Form.js?v=12.44.456';
+import { DropdownManager } from './modules/components/Dropdown.js?v=12.44.456';
+import { ViewManagerInstance } from './modules/views/ViewManager.js?v=12.44.456';
+import { MyEventsViewInstance } from './modules/views/MyEvents.js?v=12.44.456';
+import { AdminViewInstance } from './modules/views/Admin.js?v=12.44.456';
+import { EventConfigViewInstance } from './modules/views/EventConfig.js?v=12.44.456';
+import { SystemViewInstance } from './modules/views/System.js?v=12.44.456';
+import { ApiServiceInstance } from './modules/services/ApiService.js?v=12.44.456';
+import { AuthServiceInstance } from './modules/services/AuthService.js?v=12.44.456';
+import { EventServiceInstance } from './modules/services/EventService.js?v=12.44.456';
+import { GuestServiceInstance } from './modules/services/GuestService.js?v=12.44.456';
 
-// DEBUG V12.44.452 - Si ves esto, el código nuevo se cargó
-console.log('[INIT] app.js version 12.44.452 loaded');
-console.log('[MODULES] Todos los módulos cargados v12.44.452');
+// DEBUG V12.44.456 - Si ves esto, el código nuevo se cargó
+console.log('[INIT] app.js version 12.44.456 loaded');
+console.log('[MODULES] Todos los módulos cargados v12.44.456');
 
 /**
  * MASTER SCRIPT
- * Version: V12.44.363 (Neutral Dark)
+ * Version: V12.44.456 (Neutral Dark)
  * Author: Carlos
  * 
  * Description: Sistema modular de gestión de asistencia con diseño Chrome Style.
@@ -41,7 +41,7 @@ console.log('[MODULES] Todos los módulos cargados v12.44.452');
  */
 window.LS = LS;
 window.lazyLoad = lazyLoad;
-const VERSION = '12.44.366';
+const VERSION = '12.44.456';
 console.log(`CHECK V${VERSION}: Iniciando Sistema Modular...`);
 
 // --- VERIFICACIÓN INMEDIATA DE VERSIÓN CARGADA (SIMPLIFICADA) ---
