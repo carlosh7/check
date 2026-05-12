7f056afe4c910d6a9f0a5a6b5a4f3610447f8c80
carlosh7 <carlosh7@gmail.com>
2026-05-11 21:27:49 -0500

    audit: limpiar codigo muerto - paypal sdk, google-sheets, lang js legacy (v12.44.762)

--- public/js/i18n.js
+++ public/js/i18n.js
@@ -16,7 +16,7 @@ let translations = {};
 
 async function loadLang(lang) {
     try {
-        const res = await fetch('/js/lang/' + lang + '.json?v=12.44.761');
+        const res = await fetch('/js/lang/' + lang + '.json?v=12.44.762');
         if (res.ok) {
             translations = await res.json();
         } else {
